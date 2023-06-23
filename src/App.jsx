41d35d0c44b6b57ee9fd93ff384ae3418cf6fa65
anshid haneef <anshidhaneefa710@gmail.com>
2023-06-23 17:52:29 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,68 @@
 import { useEffect, useState } from "react";
 import "./App.scss";
 
-const gameIcons = ['🌍','🔎','🔥','🐱‍🏍','✨','👀','🎈','🛒','🍔','🍌','🚽','🚀']
+const gameIcons = [
+  "🌍",
+  "🔎",
+  "🔥",
+  "🐱‍🏍",
+  "✨",
+  "👀",
+  "🎈",
+  "🛒",
+  "🍔",
+  "🍌",
+  "🚽",
+  "🚀",
+];
 
 function App() {
-const [pieces,setPieces]=useState([])
+  const [pieces, setPieces] = useState([]);
+  const gameLogic = () => {
+    const duplicateGameIcons = [...gameIcons, ...gameIcons];
 
-
-  const gameLogic = ()=>{
-    const duplicateGameIcons = gameIcons.concat(gameIcons)
-
-    const newGameIcons = []
-    while(newGameIcons.length<duplicateGameIcons.length*2){
-      const randomIndex = Math.floor(Math.random()*duplicateGameIcons.length)
-         
+    const newGameIcons = [];
+    while (newGameIcons.length < gameIcons.length*2 ) {
+      const randomIndex = Math.floor(Math.random() * duplicateGameIcons.length);
       newGameIcons.push({
-        emoji:duplicateGameIcons[randomIndex],
-        flipped:false,
-        solved:false,
-        position:newGameIcons.length
-      }) 
-      duplicateGameIcons.splice(randomIndex,1)
+        emoji: duplicateGameIcons[randomIndex],
+        flipped: false,
+        solved: false,
+        position: newGameIcons.length,
+      });
+      duplicateGameIcons.splice(randomIndex, 1);
     }
-    setPieces(newGameIcons)
-  }
+    setPieces(newGameIcons);
+  };
 
-  useEffect(()=>{
-    gameLogic()
-  },[])
+  useEffect(() => {
+    gameLogic();
+  }, []);
+
+  function handleActive(data){
+   const newPices = pieces.map(piece =>{
+    if(piece.position === data.position){
+      piece.flipped = !piece.flipped
+    }
+    return piece;
+   })
+    setPieces(newPices)
+  }
+  
+ console.log(pieces);
 
   return (
     <>
-      <h2> Memory Game Using React </h2 >
+      <h2> Memory Game Using React </h2>
       <div className="container">
-       {pieces.map((data,index)=>(
-        <div className="flip-card" key={index} >
-        <div className="flip-card-inner">
-          <div className="flip-card-front" />
-          <div className="flip-card-back"> {data.emoji} </div>
-        </div> 
-      </div>
-      ))}
+        {pieces.map((data, index) => (
+          <div className={`flip-card ${data.flipped ? "active" : "" }`} key={index} onClick={()=>handleActive(data)} >
+            <div className="flip-card-inner">
+              <div className="flip-card-front" />
+              <div className="flip-card-back"> {data.emoji} </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
