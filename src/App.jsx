ff60c94036977bc3e13b205c7eba678b410fe983
anshid haneef <anshidhaneefa710@gmail.com>
2023-06-24 18:05:31 +0530

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,13 @@ const gameIcons = [
 ];
 
 function App() {
+
   const [pieces, setPieces] = useState([]);
   const gameLogic = () => {
     const duplicateGameIcons = [...gameIcons, ...gameIcons];
 
     const newGameIcons = [];
-    while (newGameIcons.length < gameIcons.length*2 ) {
+    while (newGameIcons.length < gameIcons.length * 2) {
       const randomIndex = Math.floor(Math.random() * duplicateGameIcons.length);
       newGameIcons.push({
         emoji: duplicateGameIcons[randomIndex],
@@ -39,24 +40,54 @@ function App() {
     gameLogic();
   }, []);
 
-  function handleActive(data){
-   const newPices = pieces.map(piece =>{
-    if(piece.position === data.position){
-      piece.flipped = !piece.flipped
-    }
-    return piece;
-   })
-    setPieces(newPices)
+  function handleActive(data) {
+    const newPices = pieces.map((piece) => {
+      if (piece.position === data.position) {
+        piece.flipped = !piece.flipped;
+      }
+      return piece;
+    });
+    setPieces(newPices);
   }
-  
- console.log(pieces);
 
-  return (
+
+  const gameLogicForFlipped = ()=>{
+     const flippedData = pieces.map(data => data.flipped )
+     console.log(flippedData);
+     if(flippedData.length ===  2){
+      setTimeout(() => {
+        if(flippedData[0].emoji === flippedData[1].emoji ){
+          // success
+        
+        }else{
+            setPieces( pieces.map(data =>{
+              if(data.position ===  flippedData[0].position||
+                data.position ===  flippedData[1].position){
+                  data.flipped = false
+                }
+                return data
+            }) )
+        } 
+      }, 800);
+     }
+  }
+
+  useEffect(()=>{
+    gameLogicForFlipped()
+  },[pieces])
+
+// console.log('pieces',pieces);
+
+return (
     <>
       <h2> Memory Game Using React </h2>
       <div className="container">
         {pieces.map((data, index) => (
-          <div className={`flip-card ${data.flipped ? "active" : "" }`} key={index} onClick={()=>handleActive(data)} >
+          <div
+            className={`flip-card ${data.flipped ? "active" : ""} `}
+            key={index}
+            onClick={() => handleActive(data)}
+          >
             <div className="flip-card-inner">
               <div className="flip-card-front" />
               <div className="flip-card-back"> {data.emoji} </div>
